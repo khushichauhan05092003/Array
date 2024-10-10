4be966ee2476afcb73142c45613a768e44274f7d
khushichauhan05092003 <khushichauhanindia@gmail.com>
2024-10-10 13:33:28 +0530

    Create Secondmaxarray.java

---
+++ Secondmaxarray.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Secondmaxarray {
+    public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("enter the size of an array");
+                int n = sc.nextInt();
+                int[] arr = new int[n];
+                System.out.println("enter the array element");
+                for (int i = 0; i < n; i++) {
+                    arr[i] = sc.nextInt();
+                }
+        int max = Integer.MIN_VALUE;//this was the generic method while arr[0] is not the generic method
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                    }
+                }
+        int smax = Integer.MIN_VALUE;//this was the generic method while arr[0] is not the generic method
+        for (int i = 0; i < n; i++)
+            if (arr[i] != max) {
+                smax = Math.max(smax,arr[i]);}
+                System.out.println(smax);
+    }
+    }
